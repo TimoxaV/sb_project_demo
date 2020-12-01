8d7c980d2439012d59b936a8ca74283e7930ec30
TimoxaV <vadim2493@gmail.com>
2020-12-01 17:39:03 +0200

    reduced list's length in test

--- src/test/java/boot/project/demo/service/impl/FileReaderServiceImplTest.java
+++ src/test/java/boot/project/demo/service/impl/FileReaderServiceImplTest.java
@@ -11,18 +11,9 @@ class FileReaderServiceImplTest {
     private static final String FILE_PATH_EMPTY_FILE = "src/test/resources/test_empty_file.csv";
     private static final String FILE_PATH_NO_FILE = "C::/docs";
     private static final List<String[]> TEST_OK = List.of(
-            new String[]{"Id", "ProductId", "UserId", "ProfileName", "HelpfulnessNumerator",
-                    "HelpfulnessDenominator", "Score", "Time", "Summary", "Text"},
-            new String[]{"1", "B001E4KFG0", "A3SGXH7AUHU8GW", "delmartian", "1", "1", "5", "1303862400",
-                    "Good Quality Dog Food", "I have bought several of the Vitality canned dog "
-                    + "food products and have found them all to be of good quality. The product "
-                    + "looks more like a stew than a processed meat and it smells better. My "
-                    + "Labrador is finicky and she appreciates this product better than  most."},
-            new String[]{"2", "B00813GRG4", "A1D87F6ZCVE5NK", "dll pa", "0", "0", "1", "1346976000",
-                    "Not as Advertised",
-                    "Product arrived labeled as Jumbo Salted Peanuts...the peanuts were actually "
-                            + "small sized unsalted. Not sure if this was an error or if the vendor "
-                            + "intended to represent the product as \"Jumbo\"."}
+            new String[]{"Id", "ProductId", "UserId", "ProfileName", "HelpfulnessNumerator"},
+            new String[]{"1", "B001E4KFG0", "A3SGXH7AUHU8GW", "delmartian", "1"},
+            new String[]{"2", "B00813GRG4", "A1D87F6ZCVE5NK", "dll pa", "0"}
     );
     private static FileReaderService fileReaderService;
 
